0af2aa3b42afb989239693fd21c37186357bdddd
TigRig <tigrig.crex@gmail.com>
2019-12-25 11:16:56 +0900

    [update] プロジェクトの typescript 化

---
+++ test/sample-test.ts
@@ -0,0 +1,11 @@
+import assert = require('assert')
+import { add } from '../index'
+
+describe('サンプルテスト', () => {
+  it('失敗するテスト', () => {
+    assert(add(3, 4) === 8)
+  })
+  it('成功するテスト', () => {
+    assert(add(3, 4) === 7)
+  })
+})
